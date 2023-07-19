d94deabaca02cf377950d7b48af63fa5ebdc035f
Denny-Ko <donghar@naver.com>
2023-07-19 19:55:51 +0900

    3)4) Form Login 인증, Form Login 인증 필터: UsernamePasswordAuthenticationFilter

--- src/main/java/io/security/basicsecurity/SecurityController.java
+++ src/main/java/io/security/basicsecurity/SecurityController.java
@@ -5,8 +5,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SecurityController {
-    @GetMapping
+    @GetMapping("/")
     public String index() {
         return "home";
+    }
+
+    @GetMapping("/loginPage")
+    public String loginPage() {
+        return "loginPage";
     }
 }
